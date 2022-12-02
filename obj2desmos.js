747c9a54b415e38322a7ed1fc74ad48883fd9ae3
Tyrcnex <tyrcnex@gmail.com>
2022-12-02 10:29:34 +0800

    Added float reduction

--- obj2desmos.js
+++ obj2desmos.js
@@ -9,11 +9,11 @@ function obj2Desmos(input) {
 
     let info = [];
     for (let i = 0; i < 3; i++) {
-        info.push(`${i === 0 ? 'x_{1}' : (i === 1 ? 'y_{1}' : 'z_{1}')}=\\left[${vertices.map(e => e[i]).join()}\\right]`);
+        info.push(`${i === 0 ? 'x_{1}' : (i === 1 ? 'y_{1}' : 'z_{1}')}=\\left[${vertices.map(e => parseFloat(e[i])).join()}\\right]`);
     }
 
     for (let i = 0; i < 3; i++) {
-        info.push(`${i === 0 ? 'f_{1}' : (i === 1 ? 'f_{2}' : 'f_{3}')}=\\left[${faces.map(e => e[i].split('/')[0]).join()}\\right]`);
+        info.push(`${i === 0 ? 'f_{1}' : (i === 1 ? 'f_{2}' : 'f_{3}')}=\\left[${faces.map(e => parseFloat(e[i].split('/')[0])).join()}\\right]`);
     }
 
     return info.join('\n');
