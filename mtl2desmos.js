b322daae55b3ff5d014d9dfc7ad61680a86da744
Tyrcnex <tyrcnex@gmail.com>
2023-01-10 17:18:54 +0800

    Update mtl2desmos.js

--- mtl2desmos.js
+++ mtl2desmos.js
@@ -5,7 +5,6 @@ function mtl2Desmos(mtlInput, objInput) {
         if (e[0] === 'f') newE = triangulate(e).map(q => `f ${q.join(' ')}`).join('\n');
         return newE;
     }).join('\n').split(/\r?\n/);
-    let colorArray = [];
 
     let materials = [];
     let cmMtl = ``;
@@ -23,14 +22,19 @@ function mtl2Desmos(mtlInput, objInput) {
     }
 
     let cmObj = ``;
+    let facesArray = [];
     for (let line of objArray) {
         if (line.startsWith('usemtl')) cmObj = line.replace('usemtl ', '');
         if (cmObj.length > 0 && line.charAt(0) === 'f') {
-            colorArray.push(materials.find(e => e.name === cmObj).kd);
+            facesArray.push(materials.map(e => e.name).indexOf(cmObj)+1);
         }
     }
     
-    let dataColor = colorArray[0].map((_, colIndex) => colorArray.map(row => row[colIndex]));
-    let rawColor = dataColor.map((e,i) => `C_{${"rgb"[i]}}=\\left[${e.join(',')}\\right]`).join('\n');
-    return rawColor;
+    let cr = materials.map(e => e.kd[0]);
+    let cg = materials.map(e => e.kd[1]);
+    let cb = materials.map(e => e.kd[2]);
+
+    let returnString = `c_{r}=\\left[${cr.join(',')}\\right]\nc_{g}=\\left[${cg.join(',')}\\right]\nc_{b}=\\left[${cb.join(',')}\\right]\nc_{i}=\\left[${facesArray.join(',')}\\right]`;
+
+    return returnString;
 }
